b59d6f21f34edf842faf099920298ab842caa50c
Nitin B <nitinpune55@gmail.com>
2017-11-26 20:38:33 +0530

    new file added

---
+++ src/test/java/com/nitin/test/SpringBootHelloWorldTests.java
@@ -0,0 +1,16 @@
+package com.nitin.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public abstract class SpringBootHelloWorldTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
